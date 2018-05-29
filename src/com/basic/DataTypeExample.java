8b0a91d46f547e6e794c830d04ac0cbcc5275e92
murugavsvakn <murugavsvakn@gmail.com>
2018-05-29 15:18:25 +0530

    datatype examples

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class DataTypeExample {
+
+	public static void main(String[] args) {
+		
+		String firstName= "Murugan";
+		String lastName= "Krishanmoorthy";
+		
+		System.out.println(firstName+" " + lastName);
+	}
+}
